0172af1afb5b0bbf83c98e95a31ede99d11cc684
sygaco <larrasygaco@gmail.com>
2024-12-15 02:54:30 +0800

    Create usercontext.jsx

---
+++ taskup/src/components/usercontext.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useState, useContext } from "react";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    isAuthenticated: false,
+    role: null, // 'user' or 'admin'
+  });
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
