9200929f94d1c5661d45a0e728e888d881857927
sygaco <larrasygaco@gmail.com>
2024-12-15 21:33:38 +0800

    backend index.js

--- taskup-backend/index.js
+++ taskup-backend/index.js
@@ -1,17 +1,27 @@
 const express = require('express');
-const userRoutes = require('./routes/users');
+const userRoutes = require('./routes/login');
+const cors = require('cors');
 
 const app = express();
 const port = 3001;
 
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: 'GET,POST',
+  allowedHeaders: 'Content-Type',
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
-app.use('/users', userRoutes);
+app.use('/api', userRoutes);
 
+// Optional: You can keep this if you want a simple root endpoint
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+  res.send('It works!');
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
