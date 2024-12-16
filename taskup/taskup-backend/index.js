75654701b7a1311daf65b7abef21bd42d451368e
sygaco <larrasygaco@gmail.com>
2024-12-16 17:53:39 +0800

    update index.js

--- taskup/taskup-backend/index.js
+++ taskup/taskup-backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const profileRoutes = require('./routes/profile');
 const loginRoutes = require('./routes/loginRoute');
 const signupRoutes = require('./routes/signupRoute');
+const userProfileRoute = require('./routes/userProfileRoute');
+const adminProfileRoute = require('./routes/adminProfileRoute');
 
 const app = express();
 const port = 3001;
@@ -16,9 +17,10 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use('/profile', profileRoutes);
 app.use('/login', loginRoutes);
 app.use('/signup', signupRoutes);
+app.use('/userProfile', userProfileRoute);
+app.use('/adminProfile', adminProfileRoute);
 
 app.get('/', (req, res) => {
   res.send('API is running');
